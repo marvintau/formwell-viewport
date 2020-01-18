706cc4f61948db9ee30b0bd6a3aac351a7fc5f01
marvintau <nightvista@gmail.com>
2020-01-18 23:48:18 +0800

    Updates

---
+++ precache-manifest.8d2f868b23c81ea6850eac137d10fda1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17863924d89fa1e34dfa481e3dfb97cf",
+    "url": "/formwell-viewport/index.html"
+  },
+  {
+    "revision": "c78978a16f1676615cbd",
+    "url": "/formwell-viewport/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c78978a16f1676615cbd",
+    "url": "/formwell-viewport/static/js/2.0e89e6f7.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/formwell-viewport/static/js/2.0e89e6f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "74b8a862afc281ad4783",
+    "url": "/formwell-viewport/static/js/main.086015f5.chunk.js"
+  },
+  {
+    "revision": "328d45de3bf97ad16555",
+    "url": "/formwell-viewport/static/js/runtime-main.febe910f.js"
+  },
+  {
+    "revision": "e8c1ef84697d2b2df982f57de97ee6f5",
+    "url": "/formwell-viewport/static/media/filter.e8c1ef84.svg"
+  },
+  {
+    "revision": "199eee9756f2a7e6bdd67069a3956b94",
+    "url": "/formwell-viewport/static/media/sort-ascending.199eee97.svg"
+  }
+]);
